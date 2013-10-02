f72d37ddbe5a9447aad8277d782719a4f133095d
Skrzypek <maskrz@gmail.com>
2013-10-02 14:37:09 +0200

    initial repair

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZPI::Application.config.secret_key_base = '01f12b8bcc71a91d35610819e35df4718d2b80a497c78a2a775fcb446abd6c0224daa82970a41ed942676947fe3b0dc96fdda9ad7f9e5b7657162cc06535dc4d'
