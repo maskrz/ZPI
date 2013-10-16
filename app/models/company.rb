45f1b4b6bf2587e30e951f8f0c158c4f66ecd200
Tomasz Kopacki <tomash.pl@gmail.com>
2013-10-16 22:36:57 +0200

    fix to deploy

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,6 @@
 class Company < ActiveRecord::Base
-  has_many :analisies, :temp_analisies, :companys_indices
+  has_many :analisies
+  has_many :temp_analisies
+  has_many :companys_indices
   has_many :indices, :through => :companys_indices
 end
