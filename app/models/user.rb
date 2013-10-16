b013c06e95a728a94dd302512fff20814bd2717d
Tomasz Kopacki <tomash.pl@gmail.com>
2013-10-16 14:18:02 +0200

    user entity improvement

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  has_many :user_analyses, :messages
+  has_many :messages
+  has_many :user_analyses
+  has_many :analisies, :through => :user_analyses
   
   def authenticate (pass)
     self.password == pass
--- app/models/user_analysis.rb
+++ app/models/user_analysis.rb
@@ -1,3 +1,4 @@
 class UserAnalysis < ActiveRecord::Base
-  has_one :user
+  belongs_to :user
+  belongs_to :analisy
 end
