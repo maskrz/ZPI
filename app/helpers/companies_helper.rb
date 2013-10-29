66fe14a49920881cfd9a75a32d530e36d379287b
Tomasz Kopacki <tomash.pl@gmail.com>
2013-10-29 11:53:26 +0100

    add fetch method for getting companies from the index

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,2 +1,9 @@
 module CompaniesHelper
+  require 'open-uri'
+  def self.fetch_companies(index_name)
+    response =  open(URI.parse('http://www.gpw.pl/ajaxindex.php?action=GPWCompanySearch&start=listForIndex&listTemplateName=GPWCompanySearch/ajaxList_PL&index='+index_name)).read
+    
+    pattern = />.*([A-Z]{3}).*\s*?.*\/(.*)\/">(.*)<\/a/
+    companies_temp = response.scan(pattern).to_a
+  end
 end
