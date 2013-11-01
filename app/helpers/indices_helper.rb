7a17ef0e6f16f47b7e48853c45356c0088abf515
Tomasz Kopacki <tomash.pl@gmail.com>
2013-11-01 10:26:31 +0100

    indices fetching from gpw server

--- app/helpers/indices_helper.rb
+++ app/helpers/indices_helper.rb
@@ -1,2 +1,8 @@
 module IndicesHelper
+  require 'open-uri'
+  def self.fetch_indices
+    response =  open(URI.parse('http://www.gpw.pl/indeksy_gieldowe')).read    
+    pattern = /left"><a href=".{,83}">([A-Za-z0-9\-\.]{,10})/
+    response.scan(pattern).to_a.flatten(1)
+  end
 end
