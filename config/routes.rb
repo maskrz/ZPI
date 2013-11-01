7ba571ecc12ae7a50a02278673e3fd03de68d37f
Tomasz Kopacki <tomash.pl@gmail.com>
2013-11-01 10:27:47 +0100

    routing refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,86 +1,17 @@
 ZPI::Application.routes.draw do
-  resources :cindices
-
   root :to => 'home#index'
+  resources :analisies, :temp_analisies, :companies, :company, :indices, :index, :user_analyses, :messages, :users, :cindices
+    
+  scope :page, :controller => :static_pages do
+      get 'about'
+      get 'contact'
+      get 'media'
+      get 'home'
+  end
   
-  get 'index/get_companies' => 'indices#get_companies'
-  get 'fetch_companies' => 'companies#fetch_companies'
-  
-  resources :analisies
-
-  resources :temp_analisies
-
-  resources :companies
-  
-  resources :company
-
-  resources :indices
-  
-  resources :index
-  
-  resources :user_analyses
-
-  resources :messages
-
-  resources :users
-  
-  
-  get "static_pages/about"
-  get "static_pages/media"
-  get "static_pages/contact"
-  get "home/home"
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-  
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  scope :ajax, :controller => :ajax do
+    # get 'get_companies'
+    get 'fetch_companies'
+    get 'fetch_indices'
+  end
 end
