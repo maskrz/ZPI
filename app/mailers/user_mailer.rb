d6fb90c8b7a50e1bc7ac904f5362f7d6ea9e4caa
Tomasz Kopacki <tomash.pl@gmail.com>
2013-11-06 00:01:12 +0100

    mailer for email account confirmation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,13 @@ class UserMailer < ActionMailer::Base
   
   def after_sign_up(user)
     @user = user
-    @url  = 'http://gpwanalizer.pl/auth/confirm_email?token='+@user.registration_hash
+    @url  = 'http://gpwanalizer.pl'+auth_confirm_email_path+'?token='+@user.registration_hash
     mail(to: @user.email, subject: 'Witaj w aplikacji GPW Analizer')
+  end
+  
+  def forgot_password(user)
+    @user = user
+    @url  = 'http://gpwanalizer.pl'+auth_confirm_email_path+'?token='+@user.registration_hash
+    mail(to: @user.email, subject: 'Przypomnienie GPW Analizer')
   end
 end
