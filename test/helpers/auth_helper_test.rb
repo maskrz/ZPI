aeacde043eaa6b2104fa17a260fb7700bb76898c
Maciej Skrzypinski <maskrz@gmail.com>
2013-11-06 02:02:29 +0100

    tests login, register

--- test/helpers/auth_helper_test.rb
+++ test/helpers/auth_helper_test.rb
@@ -1,4 +1,34 @@
 require 'test_helper'
 
 class AuthHelperTest < ActionView::TestCase
+  setup do
+    @user = users(:one)
+  end
+  
+  test "sign_in" do
+    sign_in_in(@user)
+    @user2 = User.where(id: session[:user_id]).take
+    assert_equal @user2.email, "test@gmail.com"
+  end
+  
+  test "signed in? success" do
+    sign_in_in(@user)
+    assert signed_in?
+  end
+  
+  test "signed in? failure" do
+    assert_equal false, signed_in?
+  end
+  
+  test "sign out" do
+    sign_in_in(@user)
+    sign_out
+    assert_equal false, signed_in?
+  end
+  
+  test "current user" do
+    sign_in_in(@user)
+    assert_equal @user, current_user
+  end
+    
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  setup do
+    @user = users(:one)
+  end
+  
+  #test "after_sign_up" do 
+   # @expected.from = 'no-reply@gpwanalizer.pl' 
+   # @expected.to = 'test@gmail.com' 
+   # @expected.subject = "Witaj w aplikacji GPW Analizer" 
+   # 
+   # @expected.body = read_fixture('after_sign_up') 
+   # @expected.date = Time.now 
+   # puts UserMailer.after_sign_up(@user).encoded 
+    #assert_equal @expected.encoded, UserMailer.after_sign_up(@user).encoded 
+  #end
 end
