8a12113f6de869191372b8bc02c87e4ca268a746
bartkotrys <bkotrys91@gmail.com>
2013-11-12 23:47:02 +0100

    tabs size in javascript

--- app/assets/javascripts/analisies.js
+++ app/assets/javascripts/analisies.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
   	$('#rootwizard').bootstrapWizard({'tabClass': 'nav nav-pills'});
+  	
+  	if(GPW.Util.checkIfExists('.wizard')) {
+  		$('.wizard .nav-tabs').each(function(){
+  			var items = $('li', this);
+  			var itemsNum = items.length;
+  			var percentageWidth = Math.round(100/itemsNum*100000000)/100000000;
+  			items.css('width',percentageWidth+'%');
+  		});
+  	}
 });
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
 	GPW.View.Home.setMainContentPosition();
 
 	GPW.View.setTooltip('#user_box .nav .dropdown-toggle', { placement : 'top' });
+	GPW.View.setTooltip('#analysis_header .tooltip-element', { placement : 'top' });
 });
 
 $(window).resize(function(){
