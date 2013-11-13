8187ba1cee62cc0667ced2e6234a58f51920243a
Tomasz Kopacki <tomash.pl@gmail.com>
2013-11-13 15:31:04 +0100

    javascript refactor

---
+++ app/assets/javascripts/ajax.js
@@ -0,0 +1,13 @@
+var GPW = GPW || {};
+
+GPW.Ajax = {};
+GPW.Ajax.getCompanies = function() {
+	$('select#indices').change(function() {
+		var selectedIndex = $(this).val();
+		$.get('/index/get_companies', {
+			'index_id' : selectedIndex
+		}, function(data) {
+			console.log(data);
+		});
+	});
+};
--- app/assets/javascripts/analisies.js
+++ app/assets/javascripts/analisies.js
@@ -1,12 +1,20 @@
-$(document).ready(function() {
-  	$('#rootwizard').bootstrapWizard({'tabClass': 'nav nav-pills'});
+var GPW = GPW || {};
+
+GPW.Analysis = {};
+
+GPW.Analysis.init = function(){
+	GPW.Analysis.wizard();
+};
+
+GPW.Analysis.wizard = function() {
+  	$('.wizard').bootstrapWizard({'tabClass': 'nav nav-pills'});
   	
-  	if(GPW.Util.checkIfExists('.wizard')) {
+  	if(GPW.Common.checkIfExists('.wizard')) {
   		$('.wizard .nav-tabs').each(function(){
   			var items = $('li', this);
   			var itemsNum = items.length;
   			var percentageWidth = Math.round(100/itemsNum*100000000)/100000000;
   			items.css('width',percentageWidth+'%');
   		});
-  	}
-});
+  	};
+};
