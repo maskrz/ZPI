ad4ff5ca64765fe3d50402e7b9c374c3506fa003
Tomasz Kopacki <tomash.pl@gmail.com>
2013-11-14 10:05:34 +0100

    vendor files reorganized

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,9 @@
 //= require jquery
 //= require jquery_ujs
-//= require typeahead.min
-//= require bootstrap
+//= require typehead/typeahead.min
+//= require bootstrap/bootstrap.min
 //= require jquery.bootstrap.wizard
-//= require jquery.raty/raty/lib/jquery.raty.min.js
+//= require jquery-raty/jquery.raty.min
 //= require_directory .
 
 GPW.Analysis.init();
