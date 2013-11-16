7ad09d8cd47eb584ba3a616c3c504e7363158638
Maciej Skrzypinski <maskrz@gmail.com>
2013-11-16 19:08:59 +0100

    active admin small changes

--- app/models/analisy.rb
+++ app/models/analisy.rb
@@ -2,4 +2,9 @@ class Analisy < ActiveRecord::Base
   belongs_to :company
   has_many :user_analyses
   has_many :users, :through => :user_analyses
+  
+  def display_name
+    self.period.to_s + " "+self.date.to_s+" "+self.company.name + " " +self.medium_rate.to_s
+  end
+  
 end
