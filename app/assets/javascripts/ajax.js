b5ff5a8dc0b9dccd367fd3c7d560a747e01c051a
Tomasz Kopacki <tomash.pl@gmail.com>
2013-11-17 19:15:14 +0100

    file refactor

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -2,13 +2,9 @@ var GPW = GPW || {};
 
 GPW.Ajax = {};
 GPW.Ajax.getIndices = function() {
-	return $.get('/ajax/get_indices', {}, function(data) {
-		indices = data;
-	});
+	return $.get('/ajax/get_indices');
 };
 GPW.Ajax.getCompanies = function(indices) {
-	return $.get('/ajax/get_companies', { 'indices' : indices}, function(data) {
-		companies = data;
-	});
+	return $.get('/ajax/get_companies', { 'indices' : indices});
 };
 
