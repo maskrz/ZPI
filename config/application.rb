efd0e4f137e3cfa18cf3c382da89bccf888ccb1e
Tomasz Kopacki <tomash.pl@gmail.com>
2013-11-19 21:15:06 +0100

    assets reorganization

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module ZPI
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :pl
-    config.assets.paths << "#{Rails}/vendor/assets/fonts"
+    config.assets.paths << "#{Rails}/app/assets/fonts"
+
   end
 end
