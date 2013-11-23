34e710b81d21ea8c8710166bb2f78e34185e96a9
Tomasz Kopacki <tomash.pl@gmail.com>
2013-11-23 21:36:45 +0100

    redirect to analysis history after log in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    redirect_to main_path if user_signed_in?
+    redirect_to wall_path if user_signed_in?
   end
 
   def main
@@ -8,6 +8,7 @@ class HomeController < ApplicationController
   end
 
   def wall
-   @analysies = current_user.get_analysies_history
+    redirect_to root_path unless user_signed_in?
+    @analysies = current_user.get_analysies_history
   end
 end
