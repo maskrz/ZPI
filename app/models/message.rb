10162c23494305a8c131fd0ad3bc02ca7db87b36
Tomasz Kopacki <tomash.pl@gmail.com>
2013-12-02 22:01:11 +0100

    message model update

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,8 @@ class Message < ActiveRecord::Base
   has_one :sender, :class_name => "User", :foreign_key => "sender_id"
   has_one :reciver, :class_name => "User", :foreign_key => "reciver_id"
   
-  validates_presence_of :content
+  validates :content, :presence =>true
+  validates :reciver_id, :presence =>true
   
   def is_sender?(id)
     id == self.sender_id
