53d87dc8de39ea26d1044cafaca33e3da54a6eee
Tomasz Kopacki <tomash.pl@gmail.com>
2013-12-03 09:47:14 +0100

    js refacor

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -28,69 +28,81 @@ GPW.Messages.countLoadedMessages = function () {
 };
 
 GPW.Messages.loadMessages = function() {
-	if(GPW.Common.checkIfExists('#messages-load-status')) {
-		GPW.Messages.batch_start = 2;
-		
-		var otherId = $('.conversation').data('other-id'),
-			otherName = $('.conversation').data('other-name'),
-			meId = $('.conversation').data('me-id'),
-			meName = $('.conversation').data('me-name');
+	var loadStatus = '#messages-load-status';
+	if(GPW.Common.checkIfExists(loadStatus)) {
+		var chat = '.chats';
+		var meId = $('.conversation').data('me-id'),
+		otherId = $('.conversation').data('other-id');
 
-			$('#messages-load-status').hide(0);
+		GPW.Messages.batch_start = 2;
+		$(loadStatus).hide(0);
 			
-		$('.chats').scroll(function(e) {
+		$(chat).scroll(function(e) {
 			if(e.target.scrollTop == 0) {
-				$('#messages-load-status').html('Wczytuję wiadomości...').fadeIn(200);
+				$(loadStatus).html('Wczytuję wiadomości...').fadeIn(200);
 				var data = { 'batch_start' : GPW.Messages.batch_start, 'interlocutor_id' : otherId };
 				
 				var beforeLoadHeight = 0;
-				$('.chats li').each(function(){
+				$('li', chat).each(function(){
 					beforeLoadHeight += $(this).outerHeight();
 				});
 				
 				$.post('/ajax/get_messages', data, function(response){
-					GPW.Messages.batch_start++;
-					for(var i = 0; i < response.length; i++) {
-						var message_data = response[i];
-						var message = $('<li/>');
-						
-						var message_class;
-						var content;
-						if(message_data.sender_id == otherId) {
-							message.addClass('by-other');
-							message.append('<div class="avatar pull-left"><span class="glyphicon glyphicon-user"></span></div>');
-							content = $('<div class="chat-content"/>');
-							content.append('<div class="chat-meta">'+otherName+' <span class="pull-right">'+message_data.created_at+'</span></div>');
-						
-						} else {
-							message.addClass('by-me');
-							message.append('<div class="avatar pull-right"><span class="glyphicon glyphicon-user"></span></div>');
-							content = $('<div class="chat-content"/>');
-							content.append('<div class="chat-meta">'+message_data.created_at+' <span class="pull-right">'+meName+'</span></div>');
-						}
-						content.append('<p>'+message_data.content+'</p>');
-						content.appendTo(message);
-						message.append('<div class="clearfix"></div>');
-						$('.conversation .chats').prepend(message);
-					}
-					
-					$('#messages-load-status').html('Wczytuję wiadomości...').fadeOut(200);
+					$(loadStatus).html('Wczytuję wiadomości...').fadeOut(200);
+					GPW.Messages.insertAll(response);
 					
 					var afterLoadHeight = 0;
-					$('.chats li').each(function(){
+					$('li', chat).each(function(){
 						afterLoadHeight += $(this).outerHeight();
 					});
 					
-					$('.conversation .chats').animate({ scrollTop : afterLoadHeight - beforeLoadHeight }, 0);
+					$(chat).animate({ scrollTop : afterLoadHeight - beforeLoadHeight }, 0);
 				
 					if(GPW.Messages.allMessagesLoaded()) {
-						$('.chats').unbind('scroll');
-						$('#messages-load-status').html('Wczytano wszystkie wiadomości.').fadeIn(200);
+						$(chat).unbind('scroll');
+						$(loadStatus).html('Wczytano wszystkie wiadomości.').fadeIn(200);
 					}
 				});
 			}
 		});
 	}
+};
+
+GPW.Messages.insertAll = function(data) {
+	var chat = '.chats';
+	GPW.Messages.batch_start++;
+	for(var i = 0; i < data.length; i++) {
+		var message_data = data[i];
+		var message = GPW.Messages.create(message_data);
+		$(chat).prepend(message);
+	}
+};
+
+GPW.Messages.create = function(data) {
+	var message = $('<li/>');
+	var content;
+	
+	var otherName = $('.conversation').data('other-name'),
+		meName = $('.conversation').data('me-name');
+	var meId = $('.conversation').data('me-id'),
+		otherId = $('.conversation').data('other-id');
+	
+	if(data.sender_id == otherId) {
+		message.addClass('by-other');
+		message.append('<div class="avatar pull-left"><span class="glyphicon glyphicon-user"></span></div>');
+		content = $('<div class="chat-content"/>');
+		content.append('<div class="chat-meta">'+otherName+' <span class="pull-right">'+data.created_at+'</span></div>');
+	
+	} else {
+		message.addClass('by-me');
+		message.append('<div class="avatar pull-right"><span class="glyphicon glyphicon-user"></span></div>');
+		content = $('<div class="chat-content"/>');
+		content.append('<div class="chat-meta">'+data.created_at+' <span class="pull-right">'+meName+'</span></div>');
+	}
+	content.append('<p>'+data.content+'</p>');
+	content.appendTo(message);
+	message.append('<div class="clearfix"></div>');
+	return message;
 };
 
 GPW.Messages.allMessagesLoaded = function() {
