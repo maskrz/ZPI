d287083d6763a62400a9e42846da850ed6b4d50b
Tomasz Kopacki <tomash.pl@gmail.com>
2013-12-04 08:48:59 +0100

    user profile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
   def index
-    redirect_to wall_path if user_signed_in?
+    if user_signed_in?
+      if current_user.has_analysis?
+        redirect_to wall_path
+      else
+        redirect_to main_path
+      end
+    end
   end
 
   def main
@@ -11,5 +17,36 @@ class HomeController < ApplicationController
     redirect_to root_path unless user_signed_in?
     @analysis = current_user.get_analysies_history
     #render json: @analysis
+  end
+  
+  def user_edit
+    require 'digest/md5'
+    require 'open-uri'
+    
+    @user = current_user
+    if params[:update]
+      unless params[:user][:password].blank?
+        @user.password = params[:user][:password]
+        @user.password_confirmation = params[:user][:password_confirmation]
+      end
+      unless params[:user][:avatar].blank?
+        if params[:user][:avatar].instance_of?(String)
+          avatar_url = params[:user][:avatar]
+          @user.avatar = open(URI.parse(avatar_url))
+        else
+          @user.avatar = params[:user][:avatar]
+        end
+      end
+      @user.first_name = params[:user][:first_name]
+      @user.last_name = params[:user][:last_name]
+      
+      if @user.valid?
+        @user.password = @user.password_confirmation = Digest::MD5.hexdigest(params[:user][:password])
+        @user.save
+        redirect_to '/home/user_edit', success: 'Zapisano zmiany!'
+      else
+        render json: params[:user][:avatar]
+      end 
+    end
   end
 end
